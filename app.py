14e4e6465a7c7a69608bae33b7f5c9e98a0dffa8
César Augusto Sibila <66441784+Czar210@users.noreply.github.com>
2024-06-25 18:49:39 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ st.set_page_config(page_title="É os guri ntemcomo Gestão de Produtos de Superm
 # Adicionar imagem no topo
 st.image("https://cdn.ome.lt/EWlvzO0sMGgiSsy0CbNtIkyEJ1A=/1200x630/smart/extras/conteudos/kung_fu_panda.jpg", use_column_width=True)  # Substitua o URL pelo caminho da sua imagem
 
-st.markdown("<h1 style='text-align: center; color: #4CAF50;'>🐼 POODRAGÃOGUERREIRO Gestão de Produtos de Supermercados 🛒</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #4CAF50;'>🐼 É os guri ntemcomo Gestão de Produtos de Supermercados 🛒</h1>", unsafe_allow_html=True)
 
 # Busca de Produtos
 st.markdown("<h2 style='color: #FF5722;'>🔍 Buscar Produtos</h2>", unsafe_allow_html=True)
